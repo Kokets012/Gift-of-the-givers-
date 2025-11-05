c00850e895815639583a01d2fb9e5f0ca6b04a76
Kokets012 <131242308+Kokets012@users.noreply.github.com>
2025-11-05 09:45:41 +0200

    Add files via upload

---
+++ DAFWebApp/Data/DAFDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DAFWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DAFWebApp.Data
+{
+    public class DAFDbContext : DbContext
+    {
+        public DAFDbContext(DbContextOptions<DAFDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Incident> Incidents { get; set; }
+        public DbSet<Donation> Donations { get; set; }
+        public DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<VolunteerTask> VolunteerTasks { get; set; } 
+    }
+}
---
+++ DAFWebApp/Migrations/20251003022247_InitialCreate.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAFWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donations",
+                columns: table => new
+                {
+                    DonationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ResourceName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    DonorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateDonated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ContactInfo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donations", x => x.DonationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Volunteers",
+                columns: table => new
+                {
+                    VolunteerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactInfo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Volunteers", x => x.VolunteerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VolunteerTasks",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VolunteerTasks", x => x.TaskId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incidents",
+                columns: table => new
+                {
+                    IncidentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateReported = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incidents", x => x.IncidentId);
+                    table.ForeignKey(
+                        name: "FK_Incidents_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VolunteerVolunteerTask",
+                columns: table => new
+                {
+                    VolunteerTasksTaskId = table.Column<int>(type: "int", nullable: false),
+                    VolunteersVolunteerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VolunteerVolunteerTask", x => new { x.VolunteerTasksTaskId, x.VolunteersVolunteerId });
+                    table.ForeignKey(
+                        name: "FK_VolunteerVolunteerTask_VolunteerTasks_VolunteerTasksTaskId",
+                        column: x => x.VolunteerTasksTaskId,
+                        principalTable: "VolunteerTasks",
+                        principalColumn: "TaskId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VolunteerVolunteerTask_Volunteers_VolunteersVolunteerId",
+                        column: x => x.VolunteersVolunteerId,
+                        principalTable: "Volunteers",
+                        principalColumn: "VolunteerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incidents_UserId",
+                table: "Incidents",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VolunteerVolunteerTask_VolunteersVolunteerId",
+                table: "VolunteerVolunteerTask",
+                column: "VolunteersVolunteerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Donations");
+
+            migrationBuilder.DropTable(
+                name: "Incidents");
+
+            migrationBuilder.DropTable(
+                name: "VolunteerVolunteerTask");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "VolunteerTasks");
+
+            migrationBuilder.DropTable(
+                name: "Volunteers");
+        }
+    }
+}
